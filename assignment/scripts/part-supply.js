d2627b9d32b6c0aba4062485fddef2229025854d
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-06-23 17:49:06 -0500

    code is working, but is it the right outcome?

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -39,17 +39,16 @@ console.log('6. Showing supplyChanges...');
 
 for (let x = 0; x < supplyChanges.length; x++){
     console.log(supplyChanges[x]);
-}
-
-if([x] > 0){
-    console.log(`Added` [x] `parts`);
-}
-    else if([x] == 0){
+    if(supplyChanges[x]> 0){
+    console.log(`Added`, [x],`parts`);
+    }
+    else if(supplyChanges[x] == 0){
         console.log('No Change');
     }
-    else if([x] < 0){
-        console.log(`Removed` [x] `parts`);
+    else if(supplyChanges[x] < 0){
+        console.log(`Removed`, [x], `parts`);
     }
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
